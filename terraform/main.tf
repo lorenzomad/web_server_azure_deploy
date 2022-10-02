1814f5c36ab55d17720b3e1873c7e2684f7d5e28
lorenzo <lorenzo.maddalena1@gmail.com>
2022-10-02 02:18:49 +0200

    changes to the terraform /n added NSG and public IP address

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,53 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.Azuredevops.location
+  resource_group_name = azurerm_resource_group.Azuredevops.name
+
+  security_rule {
+    # allow inbound access from the virtual machines in the network
+    name                       = "allowFromVMs"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefixes    = tolist(["VirtualNetwork",  "AzureLoadBalancer"])
+    destination_address_prefix = "*"
+  }
+  
+  security_rule {
+    # lower priority deny everything else
+    name                       = "denyInternetAccess"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "${var.prefix}-publicIP"
+  resource_group_name = azurerm_resource_group.Azuredevops.name
+  location            = azurerm_resource_group.Azuredevops.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   resource_group_name = azurerm_resource_group.Azuredevops.name
