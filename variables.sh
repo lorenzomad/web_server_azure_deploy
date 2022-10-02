0f4ca51f4091db903022c030c440115d796a7013
lorenzo <lorenzo.maddalena1@gmail.com>
2022-10-02 14:10:57 +0200

    created variables bash script

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export CLIENT_ID="6a14d2f5-ce90-49cc-b0f4-3c25358d8ee5"
+export SUBSCRIPTION_ID="83ab2601-ced6-4391-b0e4-f54b470eb775"
+export CLIENT_SECRET="1YS8Q~yKRl2VHTQlcXuyHtsIJWpyf1pJ.xS2Kaxs"
+export TENANT_ID="f958e84a-92b8-439f-a62d-4f45996b6d07"
